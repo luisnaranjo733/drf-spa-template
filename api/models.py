1455880972aa82490af8ceffd5a319f3198281b4
Luis Naranjo <luisnaranjo733@gmail.com>
2017-11-26 22:08:49 -0800

    add  djpylint support and restructure hierarchy

--- api/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User, Group
-
-class ShoppingItem(models.Model):
-    item_name = models.CharField(max_length=255)
-    item_owner = models.ForeignKey(User)
--- api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from rest_framework import routers
-
-from . import views
-
-router = routers.SimpleRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'shopping', views.ShoppingListViewSet)
-urlpatterns = router.urls
