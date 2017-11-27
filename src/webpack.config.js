6c8f80bc11bd43ca1bd0ed66123f8fb9f0c4b1e6
Luis Naranjo <luisnaranjo733@gmail.com>
2017-11-26 22:20:04 -0800

    configure webpack

---
+++ src/webpack.config.js
@@ -0,0 +1,53 @@
+var path = require('path')
+var webpack = require('webpack')
+var BundleTracker = require('webpack-bundle-tracker')
+// const isReachable = require('is-reachable');
+
+// let ip = 'localhost';
+
+module.exports = {
+    //the base directory (absolute path) for resolving the entry option
+    context: __dirname,
+    entry: [
+        './assets/index'
+    ],
+    
+    output: {
+        // where you want your compiled bundle to be stored
+        path: path.resolve('./assets/bundles/'), 
+        // naming convention webpack should use for your files
+        filename: '[name]-[hash].js', 
+    },
+    
+    plugins: [
+        new BundleTracker({filename: './webpack-stats.json'}), 
+    ],
+    
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/, 
+                exclude: /node_modules/, 
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ['react']
+                        }
+                    }
+                ],
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ]
+    },
+    
+    resolve: {
+        //tells webpack where to look for modules
+        modules: ['node_modules', 'bower_components'],
+        //extensions that should be used to resolve modules
+        extensions: ['.js', '.jsx'] 
+    }   
+}
