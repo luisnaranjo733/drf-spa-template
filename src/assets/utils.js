d64f0ddea919af519a02e52b51586459adcf106a
Luis Naranjo <luisnaranjo733@gmail.com>
2017-11-26 23:21:07 -0800

    tweak with utils

---
+++ src/assets/utils.js
@@ -0,0 +1,36 @@
+import * as Cookies from "js-cookie";
+
+// Replace these promises with axios
+
+export function getRequest(url) {
+    let csrf_token = Cookies.get('csrftoken');
+
+    return fetch(url, {
+        method: "GET",
+        credentials: "same-origin",
+        headers: {
+            "X-CSRFToken": csrf_token,
+        }
+    }).then(function(response) {
+        return response.json();
+    }).catch(function(ex) {
+        console.log(`Request to ${url} failed`, ex);
+    });
+}
+
+export function postRequest(url, data) {
+    let csrf_token = Cookies.get('csrftoken');
+
+    return fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
+        headers: {
+            "X-CSRFToken": csrf_token,
+        },
+        body: data
+    }).then(function(response) {
+        return response.json();
+    }).catch(function(ex) {
+        console.log(`Request to ${url} failed`, ex);
+    });
+}
