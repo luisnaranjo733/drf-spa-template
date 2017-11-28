cc94c367081fa0fe52fe8a7f61060c4c4687abc0
Luis Naranjo <luisnaranjo733@gmail.com>
2017-11-27 22:49:17 -0800

    implement a POST method

--- src/api/views.py
+++ src/api/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.views import APIView
 
@@ -10,15 +11,24 @@ from .models import ShoppingItem
 
 
 class ShoppingItemsEndpoint(APIView):
-    def get(self, request, format=None):
+    def get(self, request):
         queryset = ShoppingItem.objects.all()
         serializer = ShoppingItemSerializer(queryset, many=True)
         return Response(serializer.data)
 
-    def post(self, request, format=None):
-        queryset = ShoppingItem.objects.all()
-        serializer = ShoppingItemSerializer(queryset, many=True)
-        return Response(serializer.data)
+    def post(self, request):
+        print(request.POST)
+        shopping_item_name = request.POST.get('item_name', None)
+
+        if not shopping_item_name:
+            response = Response(status=status.HTTP_400_BAD_REQUEST)
+            response['error'] = 'Missing POST param "item_name"'
+            return response
+
+        shopping_item = ShoppingItem.objects.create(
+            item_name=item_name,
+            item_owner=request.user
+        )
 
 class ShoppingItemsDetailEndpoint(APIView):
     def get(self, request, pk):
