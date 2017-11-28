122eddff6805cc112b8beea1da5aad6405e1a27f
Luis Naranjo <luisnaranjo733@gmail.com>
2017-11-27 21:57:53 -0800

    add form handling js

--- src/assets/js/components/HomePage.js
+++ src/assets/js/components/HomePage.js
@@ -10,6 +10,14 @@ import { SHOPPING_LIST_ENDPOINT } from '../endpoints'
 
 class HomePage extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {new_shopping_item_name: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   componentDidMount() {
     let outerThis = this;
     axios.get(SHOPPING_LIST_ENDPOINT).then(response => {
@@ -18,15 +26,42 @@ class HomePage extends Component {
 
   }
 
+  handleChange(event) {
+    this.setState({new_shopping_item_name: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    
+    let data = {
+      item_name: this.state.new_shopping_item_name
+    };
+
+    axios.post(SHOPPING_LIST_ENDPOINT, data).then(response => {
+      console.log(response);
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+
   render() {
     return (
       <div className="container">
         <h2>List of shopping items</h2>
         <ul>
-        {this.props.shopping_items.map((shopping_item, index) => (
-          <li key={index}>{shopping_item.item_name}</li>
-        ))}
+          {this.props.shopping_items.map((shopping_item, index) => (
+            <li key={index}>{shopping_item.item_name}</li>
+          ))}
         </ul>
+
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
