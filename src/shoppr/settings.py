e5dbcb4b5838c4c3bd212decbcd8bbafa9217758
Luis Naranjo <luisnaranjo733@gmail.com>
2017-11-29 01:32:33 -0800

    secure the aps to logged in users only

--- src/shoppr/settings.py
+++ src/shoppr/settings.py
@@ -141,3 +141,9 @@ WEBPACK_LOADER = {
         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
     }
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
